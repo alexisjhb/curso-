6d47811a6a41990f931e46308a70dfa5e012421b
alexisjhb <169068450+alexisjhb@users.noreply.github.com>
2024-05-13 21:25:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+System.out.println("-*-*-*-Curso de desenvolvimento de sistemas-*-*-*-");
+
+System.out.println("");  
+  
+System.out.println("----Mais o que a gente viu ate agora no curso? o que a gente viu ate agora foi o comando System.out.println esse comando ajuda a mostrar o texto que a gente quer um exemplo foi os dois trabalho que ja foi feito sobre uma sipnose de uma serie favorita e a outra sobre um artista ou album favorito.----"); 
+  }
+}
